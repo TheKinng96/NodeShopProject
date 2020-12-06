6cb7294d34ee6fc5d6ce4d2eeddd851b7fc84033
Feng Yuan Yap <yap@corekara.co.jp>
2020-12-06 11:25:03 +0900

    updated the app.js

--- app.js
+++ app.js
@@ -3,23 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorControllers = require('./controllers/error');
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// to register the dynamic content loader
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorControllers.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
