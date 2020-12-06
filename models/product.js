fb0903484ec18b386ed58136cbb05bb2c948501f
Feng Yuan Yap <yap@corekara.co.jp>
2020-12-06 11:24:21 +0900

    restructured and completed the new shop appearance

--- models/product.js
+++ models/product.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
@@ -10,24 +14,27 @@ const getProductsFromFile = cb => {
     } else {
       cb(JSON.parse(fileContent));
     }
-  })
-}
+  });
+};
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
-      })
-    })
+      });
+    });
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
-}
+};
