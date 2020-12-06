315f4196d9e5196e3220dea41953c28dc1051d53
Feng Yuan Yap <yap@corekara.co.jp>
2020-12-06 10:44:08 +0900

    moved the app logic into a MVC model logic

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorControllers = require('./controllers/error');
+
 const app = express();
 
 // to register the dynamic content loader
@@ -15,11 +17,9 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page Not Found'});
-});
+app.use(errorControllers.get404);
 
 app.listen(3000);
